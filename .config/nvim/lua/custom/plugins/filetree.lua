8deea8ff5b0144ff747f308dbf72eeb3f3625619
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-04-08 00:35:49 +0100

    Configured neo-tree
    Ignored some folders and files while forced show other
    and added auto-close when it's last window for convenience

--- .config/nvim/lua/custom/plugins/filetree.lua
+++ .config/nvim/lua/custom/plugins/filetree.lua
@@ -3,13 +3,36 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  version = "v3.*",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {}
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          hide_by_name = {
+            "node_modules",
+          },
+          always_show = {
+            '.gitignore',
+            '.env',
+          },
+          never_show = {
+            'venv',
+            '.venv',
+          },
+          never_show_by_pattern = {
+            '*.db',
+            '*.sqlite3',
+            '*.pyc',
+          }
+        }
+      }
+    }
   end,
 }
