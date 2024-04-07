d70c2c7a286f047adc79096a2f24e98b22869bb3
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-04-07 23:49:01 +0100

    added cmus configuration

---
+++ cmus/playlist.pl
@@ -0,0 +1,1 @@
+/home/spounka/Music/Rock And Metal/VA - 100 Greatest Classic Rock Songs (2019) Mp3 320kbps Quality Album [PMEDIA]/71. The Smiths - There Is a Light That Never Goes Out (2011 Remaster).mp3
