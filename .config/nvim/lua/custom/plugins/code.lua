807565de069e046b66a216dfefabac78be1b7d49
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2024-04-15 19:55:25 +0100

    CMake-tools config for running / debugging

--- .config/nvim/lua/custom/plugins/code.lua
+++ .config/nvim/lua/custom/plugins/code.lua
@@ -102,7 +102,23 @@ return
   {
     "Civitasv/cmake-tools.nvim",
     config = function()
-      require("cmake-tools").setup({})
+      require("cmake-tools").setup({
+        cmake_dap_configuration = {
+          -- console = "toggleterm"
+        },
+        cmake_runner = {
+          name = "toggleterm",
+          default_opts = {
+            quickfix = {
+              auto_close_when_success = false
+            },
+            toggleterm = {
+              direction = "tab",
+              close_on_exit = true,
+            }
+          }
+        }
+      })
     end
   },
 }
