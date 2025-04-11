51c1e0d50a9d7fe4917c154577c0e211a3e17dff
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2025-04-11 20:43:23 +0100

    fix: add evil integration and more packages

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -15,9 +15,9 @@
 (global-display-line-numbers-mode 1)
 
 ;; Font settings
-
 (set-face-attribute 'default nil :font "Iosevka Nerd Font Mono" )
-(set-frame-font "FiraCode Nerd Font Mono-18" nil t)
+(set-frame-font "Iosevka Nerd Font Mono-18" nil t)
+(add-to-list 'default-frame-alist '(font . "Iosevka Nerd Font Mono-18"))
 
 ;; Package Manager
 (defvar bootstrap-version)
@@ -42,21 +42,76 @@
 (use-package el-patch)
 
 ;; Evil mode
-(use-package evil :init (evil-mode 1))
-(use-package evil-surround)
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1)
+  (evil-set-leader 'normal (kbd "SPC"))
+  (evil-set-leader 'emacs (kbd "\\") t)
+
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-d-scroll t)
+  (setq evil-want-C-w-in-emacs-state t)
+  (setq evil-want-Y-yank-to-eol t)
+  (setq evil-indent-convert-tabs nil)
+  (setq evil-shift-round nil)
+
+  (evil-define-key 'normal 'global (kbd "gc") 'comment-line)
+  (evil-define-key nil 'global (kbd "C-j") 'evil-window-down)
+  (evil-define-key nil 'global (kbd "C-k") 'evil-window-up)
+  (evil-define-key nil 'global (kbd "C-l") 'evil-window-right)
+  ;; (evil-define-key nil 'global (kbd "C-h") 'evil-window-left)
+  (evil-define-key 'normal 'global (kbd "<leader>g") 'magit-status)
+  (evil-define-key 'normal 'global (kbd "<leader>:") 'execute-extended-command))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package evil-surround
+  :after evil
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
 (use-package evil-snipe)
 (use-package evil-numbers)
+(use-package which-key :init (which-key-mode))
 
 ;; Ivy
-(use-package ivy :init (ivy-mode 1))
+(use-package ivy :init (ivy-mode 0))
 (use-package vertico :init (vertico-mode 1))
+(use-package vertico-posframe
+  :after vertico-mode
+  :ensure t
+  :init (vertico-posframe-mode 1))
+(use-package savehist :init (savehist-mode))
 
 ;; eye candy
 (use-package all-the-icons)
 (use-package doom-modeline :init (doom-modeline-mode 1))
-(use-package doom-themes :init (load-theme 'doom-monokai-classic))
-(use-package hl-todo)
+(use-package doom-themes
+  :init (setq doom-themes-enable-bold t
+	doom-themes-enable-italic t)
+  :config
+  (load-theme 'doom-monokai-classic)
+  (doom-themes-org-config))
+
+(use-package hl-todo :config (global-hl-todo-mode 1))
 (use-package vterm)
 
 ;; Magit
 (use-package magit)
+
+;; compile-mode
+(use-package ansi-color
+  :ensure nil
+  :config
+  (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter))
+
+
