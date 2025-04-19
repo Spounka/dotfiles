b8ed7463dbd708351658a6a292dd76b08af773dd
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2025-04-19 14:06:50 +0100

    feat: add markdown mode support

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -114,3 +114,23 @@
   (add-hook 'compilation-filter-hook 'ansi-color-compilation-filter))
 
 
+;; markdown
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
+
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist
+             '("\\.\\(?:md\\|markdown\\|mkd\\|mdown\\|mkdn\\|mdwn\\)\\'" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-map (kbd "C-c C-e") #'markdown-do))
+
