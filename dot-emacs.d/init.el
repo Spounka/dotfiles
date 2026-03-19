0e4f70f3d068249504fc34e0844eaeda6cfdd36e
Nazih Boudaakkar <nazih.boudaakkar@corepulse.fr>
2026-03-19 15:50:04 +0100

    feat(emacs): add some org-mode eye candy

--- dot-emacs.d/init.el
+++ dot-emacs.d/init.el
@@ -226,28 +226,29 @@
 
 
 ;; Org mode
-;; (setq org-hide-emphasis-markers t)
-;; (use-package org-bullets
-;;     :config
-;;     (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+(setq org-hide-emphasis-markers t)
+(use-package org-bullets
+    :after org
+    :config
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-;; ;; Resize Org headings
-;; ;; (dolist (face '((org-level-1 . 1.35)
-;; ;;                 (org-level-2 . 1.3)
-;; ;;                 (org-level-3 . 1.2)
-;; ;;                 (org-level-4 . 1.1)
-;; ;;                 (org-level-5 . 1.1)
-;; ;;                 (org-level-6 . 1.1)
-;; ;;                 (org-level-7 . 1.1)
-;; ;;                 (org-level-8 . 1.1)))
-;; ;;   (set-face-attribute (car face) nil :font "Iosevka Nerd Font" :weight 'bold :height (cdr face)))
+;; Resize Org headings
+(dolist (face '((org-level-1 . 1.35)
+                (org-level-2 . 1.3)
+                (org-level-3 . 1.2)
+                (org-level-4 . 1.1)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Iosevka Nerd Font" :weight 'bold :height (cdr face)))
 
-;; ;; ;; Make the document title a bit bigger
-;; (set-face-attribute 'org-document-title nil :font "Iosevka NF" :weight
-;; 'bold :height 1.8)
+;; Make the document title a bit bigger
+(set-face-attribute 'org-document-title nil :font "Iosevka NF" :weight
+'bold :height 1.8)
 
-;; (require 'org-indent)
-;; (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+(require 'org-indent)
+(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
 
 ;; (set-face-attribute 'org-block nil            :foreground nil :inherit
 ;; 'fixed-pitch :height 0.85)
@@ -259,26 +260,21 @@
 ;; (set-face-attribute 'org-meta-line nil        :inherit '(font-lock-comment-face fixed-pitch))
 ;; (set-face-attribute 'org-checkbox nil         :inherit 'fixed-pitch)
 
-;; (add-hook 'org-mode-hook 'variable-pitch-mode)
+(add-hook 'org-mode-hook 'variable-pitch-mode)
 
-;; ;; only enable in case of latex preview problems
+(setq org-adapt-indentation t
+      org-hide-leading-stars t
+      org-hide-emphasis-markers t
+      org-pretty-entities t
+      org-ellipsis "  ·")
 
-;; ;; (plist-put org-format-latex-options :scale 2)
+(setq org-src-fontify-natively t
+	  org-src-tab-acts-natively t
+      org-edit-src-content-indentation 0)
 
-;; (setq org-adapt-indentation t
-;;       org-hide-leading-stars t
-;;       org-hide-emphasis-markers t
-;;       org-pretty-entities t
-;;       org-ellipsis "  ·")
+(add-hook 'org-mode-hook 'visual-line-mode)
 
-;; (setq org-src-fontify-natively t
-;; 	  org-src-tab-acts-natively t
-;;       org-edit-src-content-indentation 0)
-
-;; (add-hook 'org-mode-hook 'visual-line-mode)
-
-;; (use-package olivetti)
-;; (add-hook 'org-mode-hook 'olivetti-mode)
+(use-package olivetti :after org :ensure t :hook (org-mode . olivetti-mode))
 
 
 (use-package org-superstar
@@ -311,7 +307,7 @@
   :hook (org-mode . toc-org-mode)
   :config
   ;; Optional: customize maximum depth (default 2) or link style:
-  ;; (setq toc-org-href-style "github")
+  (setq toc-org-href-style "github")
   )
 
 
